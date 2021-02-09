6e2a6f461a740e432f37be1fe55c56d4212f3b92
KOSSAN38 <isak.edlund@elev.ga.ntig.se>
2021-02-09 11:08:05 +0100

    Feat: fixade lösenord och sådant roligt :)

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('login', { title: 'Buzzfed' })
+});
+
+router.post('/', function (req, res, next) {
+
+  console.log(req.body);
+
+  const usename = req.body.username;
+  const password = req.body.password;
+
+  if (password == "dinmamma") {
+    res.send('hihihihihi');
+  }
+
 });
 
 module.exports = router;
